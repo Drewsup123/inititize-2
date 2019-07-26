ace837d5392cbada4c9aa100b6527e5cddca22cf
Drew <drewsup123c@gmail.com>
2019-07-26 09:55:19 -0600

    removed some react boilerplate, and set up redux

---
+++ initize/src/redux/actions.js
@@ -0,0 +1,24 @@
+// /* 
+//   Action Types Go Here!
+//   Be sure to export each action type so you can pull it into your reducer
+// */
+// export const SIGNUP = "SIGNUP";
+// export const LOGIN = "LOGIN";
+// export const ADDBOARD = "ADD_BOARD";
+// export const ADDBOARDID = "ADDBOARDID"
+
+// export const handleLogin = (payload) => dispatch => {
+//   dispatch({type: LOGIN, payload:payload})
+// }
+
+// export const handleSignup = (payload) => dispatch => {
+//   dispatch({type: SIGNUP, payload:payload})
+// } 
+
+// export const addBoard = (payload) => dispatch => {
+//   dispatch({type:ADDBOARD, payload:payload})
+// }
+
+// export const addBoardId = payload => dispatch => {
+//   dispatch({type: ADDBOARDID, payload: payload})
+// }
---
+++ initize/src/redux/reducer.js
@@ -0,0 +1,13 @@
+// Importing actions here
+import {} from './actions';
+
+const initialState = {
+}
+
+export const reducer = (state = initialState, action) => {
+
+    switch(action.type){
+        default:
+            return state;
+    }
+}
