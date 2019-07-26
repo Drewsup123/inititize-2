d303305d3f8a7b2feed910cb0b7ef83d0ae3e2e6
Drew <drewsup123c@gmail.com>
2019-07-26 12:17:01 -0600

    prettying up the code a bit

--- initize/src/pages/SignUp.js
+++ initize/src/pages/SignUp.js
@@ -4,8 +4,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -13,7 +11,7 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
-import { Grid, AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar } from '@material-ui/core';
 
 const styles = theme => ({
     main: {
@@ -57,13 +55,14 @@ class SignUp extends Component {
         this.state = {
         isLoading: true
         }
-        }
+    }
 
     render() {
         return (
             <React.Fragment> 
             <main className={this.props.classes.main}>
             <CssBaseline />
+
             <AppBar position="absolute" color="default" className={this.props.classes.appBar}>
                 <Toolbar style={{'display':'flex','justifyContent':'center'}}>
                 <Typography variant="h4" color="primary" noWrap >
@@ -71,44 +70,53 @@ class SignUp extends Component {
                 </Typography>
                 </Toolbar>
             </AppBar>
+
             <Paper className={this.props.classes.paper} >
+
                 <Avatar className={this.props.classes.avatar}>
-                <LockOutlinedIcon />
+                    <LockOutlinedIcon />
                 </Avatar>
 
                 <form className={this.props.classes.form} onSubmit={this.handleLogin}>
-                <FormControl margin="normal" required fullWidth>
-                    <InputLabel htmlFor="email">Email Address</InputLabel>
-                    <Input id="email" name="email" autoComplete="email" autoFocus />
-                </FormControl>
-                <FormControl margin="normal" required fullWidth>
-                    <InputLabel htmlFor="password">Password</InputLabel>
-                    <Input name="password" type="password" id="password" autoComplete="current-password" />
-                </FormControl>
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={this.props.classes.submit}
-                >
-                
-                    {this.state.isLoading ? ' Sign in':'Loadingâ¦' }
-                </Button>
-                <Button
-                    fullWidth
-                    variant="contained"
-                    color="secondary"
-                    className={this.props.classes.submit}
-                >
-                Sign up
-                </Button>
+
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="email">Email Address</InputLabel>
+                        <Input id="email" name="email" autoComplete="email" autoFocus />
+                    </FormControl>
+
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <Input name="password" type="password" id="password" autoComplete="current-password" />
+                    </FormControl>
+
+                    {/* <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    /> */}
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={this.props.classes.submit}
+                    >
+                    
+                        {this.state.isLoading ? ' Sign in':'Loadingâ¦' }
+                    </Button>
+
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className={this.props.classes.submit}
+                    >
+                    Sign up
+                    </Button>
+
                 </form>
-            
+                
+                <Link to="/" ><p>Back to Homescreen</p></Link>
             </Paper>
             
             </main>
