2c5e10e19b56a25a3b33803552d704865af6ca12
Drew <drewsup123c@gmail.com>
2019-07-26 11:47:34 -0600

    added imports

--- initize/src/App.js
+++ initize/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 // Router Imports
 import { Route } from 'react-router-dom';
 import {connect} from 'react-redux';
 // Page Imports
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" render={() => {<LandingPage />}} />
-      <Route path="/login" render={() => {<Login />}} />
-      <Route path="sign-up" render={() => {<SignUp />}} />
-      <Route path="dashboard" render={() => {<Dashboard />}} />
+      <Route exact path="/" render={() => <LandingPage />} />
+      <Route path="/login" render={() => <Login />} />
+      <Route path="sign-up" render={() => <SignUp />} />
+      <Route path="dashboard" render={() => <Dashboard />} />
     </div>
   );
 }
