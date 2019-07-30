242e23252fcfd8a993a0fe453ebc2ef0c7c2c68b
Drew <drewsup123c@gmail.com>
2019-07-30 12:01:25 -0600

    finished with sortable list

--- initize/src/components/global/SideMenu.js
+++ initize/src/components/global/SideMenu.js
@@ -12,9 +12,6 @@ import TopNav from './AppBar';
 import Paper from '@material-ui/core/Paper'
 import Avatar from '@material-ui/core/Avatar';
 
-
-const drawerWidth = 240;
-
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
@@ -24,17 +21,9 @@ const useStyles = makeStyles(theme => ({
         width : "12%",
         position : "fixed",
         backgroundColor : "rgba(0,0,0,0.4)",
-    },
-    appBar: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
+        display : "flex",
+        flexDirection : "row",
+        paddingTop: "15px",
     },
     toolbar: {
         display : "flex",
@@ -43,11 +32,23 @@ const useStyles = makeStyles(theme => ({
     },
     avatar:{
         backgroundColor:"blue"
+    },
+    sidemenuBoards:{
+        width : "15%",
+        borderRight : "2px solid grey",
+        height : "100%",
+    },
+    sidemenuContent:{
+        width:"85%",
+        display:"flex",
+        flexDirection : "column",
+        alignItems : "center"
     }
 }));
 
 export default function SideMenu(props){
     const classes = useStyles();
+    const [tempArr, setTempArr] = React.useState([1,2,3,4,5,6,7])
     if(props.location.pathname === '/' || props.location.pathname === "/authenticate"){
         return null;
     }
@@ -55,39 +56,44 @@ export default function SideMenu(props){
         <React.Fragment>
             <TopNav />
             <div className={classes.sidemenu}>
-                <Divider />
-                {/* <List>
-                    {['Boards', 'Messages', 'Account Settings'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                    ))}
-                </List> */}
+                <div className={classes.sidemenuBoards}>
+                    {tempArr.map(element => <Avatar className={classes.avatar}>{element}</Avatar>)}
+                </div>
+                <div className={classes.sidemenuContent}>
+                    <Divider />
+                    {/* <List>
+                        {['Boards', 'Messages', 'Account Settings'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                        ))}
+                    </List> */}
 
-                <Paper style={{display:"flex", justifyContent : "center", alignItems : "center", flexDirection : "column", background : "grey", padding : "5%"}}>
-                    <div style={{display:"flex", justifyContent : "space-between", alignItems : "center", flexDirection : "row", width : "60%"}}>
-                        <Avatar className={classes.avatar}>DJ</Avatar>
-                        <h3>Drew Johnson</h3>
-                    </div>
+                    <Paper style={{display:"flex", justifyContent : "center", alignItems : "center", flexDirection : "column", background : "grey", padding : "2%", width : "80%"}}>
+                        <div style={{display:"flex", justifyContent : "space-between", alignItems : "center", flexDirection : "row", width : "60%"}}>
+                            <Avatar className={classes.avatar}>DJ</Avatar>
+                            <h3>Drew Johnson</h3>
+                        </div>
 
-                    <div style={{display:"flex", justifyContent : "space-between", alignItems : "center", flexDirection : "row", width : "50%"}}>
-                        <SettingsIcon />
-                        <p>Account Settings</p>
-                    </div>
-                </Paper>
+                        <div style={{display:"flex", justifyContent : "space-between", alignItems : "center", flexDirection : "row", width : "50%"}}>
+                            <SettingsIcon />
+                            <p>Account Settings</p>
+                        </div>
+                    </Paper>
 
-                <Divider />
-                <List>
-                    <ListItem button>
-                        <ListItemIcon><InboxIcon /></ListItemIcon>
-                        <ListItemText primary='Boards' />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemIcon><MailIcon /></ListItemIcon>
-                        <ListItemText primary='Messages' />
-                    </ListItem>
-                </List>
+                    <Divider />
+                    <List>
+                        <ListItem button>
+                            <ListItemIcon><InboxIcon /></ListItemIcon>
+                            <ListItemText primary='Boards' />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon><MailIcon /></ListItemIcon>
+                            <ListItemText primary='Messages' />
+                        </ListItem>
+                    </List>
+                </div>
             </div>
         </React.Fragment>
     );
--- initize/src/pages/dashboard.js
+++ initize/src/pages/dashboard.js
@@ -1,17 +1,150 @@
 import React from 'react';
+import {sortableContainer, sortableElement, sortableHandle} from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete'
+
+const DragHandle = sortableHandle(() => <span>::</span>);
+
+const SortableContainer2 = sortableContainer(({children}) => {
+    return(
+            <TableBody>
+                {children}
+            </TableBody>
+    )
+})
+
+const SortableItem2 = sortableElement(({value}) =>
+        <React.Fragment>
+            <TableRow>
+                <TableCell><DragHandle /></TableCell>
+                <TableCell component="th" scope="row">
+                    {value.title}
+                </TableCell>
+                <TableCell>{value.priority}</TableCell>
+                <TableCell>{value.user}</TableCell>
+                <TableCell>{value.status}</TableCell>
+                <TableCell>{value.description}</TableCell>
+                <TableCell>{value.notes}</TableCell>
+                <TableCell><DeleteIcon /></TableCell>
+            </TableRow>
+        </React.Fragment>
+)
 
 class Dashboard extends React.Component{
     constructor(){
         super();
         this.state = {
-
+            temp_Array : ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+            tempRows : [
+                {
+                    title : "test",
+                    priority : "critical_1",
+                    user : "Drew Johnson",
+                    status : "done",
+                    description : "test description 1",
+                    notes : "this is a note 1"
+                },
+                {
+                    title : "test2",
+                    priority : "critical_2",
+                    user : "Drew Johnson",
+                    status : "done",
+                    description : "test description 2",
+                    notes : "this is a note 2"
+                },
+                {
+                    title : "test3",
+                    priority : "critical_3",
+                    user : "Drew Johnson",
+                    status : "done",
+                    description : "test description 3",
+                    notes : "this is a note 3"
+                },
+                {
+                    title : "test4",
+                    priority : "critical_4",
+                    user : "Drew Johnson",
+                    status : "done",
+                    description : "test description 4",
+                    notes : "this is a note 4"
+                },
+            ]
         }
     }
 
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState(({temp_Array}) => ({
+            temp_Array: arrayMove(temp_Array, oldIndex, newIndex),
+        }));
+    };
+
+    onSortEnd2 = ({oldIndex, newIndex}) => {
+        this.setState(({tempRows}) => ({
+            tempRows: arrayMove(tempRows, oldIndex, newIndex),
+        }));
+    };
+
     render(){
+        const {temp_Array, tempRows} = this.state;
         return(
             <div className="dashboard">
-                
+                <Paper>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Task</TableCell>
+                                <TableCell>Priority</TableCell>
+                                <TableCell>User</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Description</TableCell>
+                                <TableCell>Notes</TableCell>
+                                <TableCell><AddIcon /></TableCell>
+                            </TableRow>
+                        </TableHead>
+
+                        <SortableContainer2 onSortEnd={this.onSortEnd2} useDragHandle lockAxis="y" lockToContainerEdges={true}>
+                            {tempRows.map((value, index) => <SortableItem2 key={index} index={index} value={value}/>)}
+                        </SortableContainer2>
+                    </Table>
+                </Paper>
+
+                {/* <Paper>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Task</TableCell>
+                                <TableCell>Priority</TableCell>
+                                <TableCell>User</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Description</TableCell>
+                                <TableCell>Notes</TableCell>
+                                <TableCell>Actions</TableCell>
+                            </TableRow>
+                        </TableHead>
+
+                        <TableBody>
+                        {tempRows.map((row, index) => (
+                            <TableRow key={index}>
+                            <TableCell component="th" scope="row">
+                                {row.title}
+                            </TableCell>
+                            <TableCell>{row.priority}</TableCell>
+                            <TableCell>{row.user}</TableCell>
+                            <TableCell>{row.status}</TableCell>
+                            <TableCell>{row.description}</TableCell>
+                            <TableCell>{row.notes}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </Paper> */}
             </div>
         );
     }
