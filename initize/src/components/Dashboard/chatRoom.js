713229df9366d7e718e128ac479eab214dc72146
Drew <drewsup123c@gmail.com>
2019-09-12 22:35:37 -0600

    getting image upload boilerplated for chatroom

--- initize/src/components/Dashboard/chatRoom.js
+++ initize/src/components/Dashboard/chatRoom.js
@@ -7,6 +7,12 @@ import Avatar from '@material-ui/core/Avatar';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import * as firebase from 'firebase';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 import {connect} from 'react-redux';
 
 class ChatRoom extends React.Component{
@@ -21,7 +27,8 @@ class ChatRoom extends React.Component{
             searchTerm : "",
             searchResults : [],
             searchLoading : false,
-            file : "",
+            file : null,
+            fileSelectOpen : false,
         }
     }
 
@@ -67,85 +74,129 @@ class ChatRoom extends React.Component{
         }
     }
 
-    uploadImage = () => {
-        
-    }
-
-    setFile = e => {
-        this.setState({file : e.target.files[0]});
+    uploadFile = () => {
+        const { file } = this.state;
+        const uploadTask = firebase.storage()
+        .ref(`/chatroomFiles/${this.props.selectedBoard.id}`)
+        .child(`${file.lastModified}${file.name}`)
+        .put(this.state.file);
+        uploadTask.on('state_changed', 
+            snapshot => {
+                let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            },
+            error => {
+                alert("There was an error uploading the image");
+            }
+            ,() => {
+                uploadTask.ref.getDownloadURL().then(downloadURL => {
+                    console.log(downloadURL);
+                    firebase.database().ref(`/boardData/${this.props.selectedBoard.id}/${this.props.match.params.subBoardId}`)
+                })
+            }
+        )
     }
 
     handleChange = e => {
         this.setState({[e.target.name] : e.target.value});
     }
 
+    handleFileSelect = e => {
+        console.log(e.target.files[0].name);
+        this.setState({ file : e.target.files[0] });
+    }
+
     scrollToBottom = () => {
         this.messagesEnd.scrollIntoView({ behavior : "smooth" })
     }
 
+    handleFileSelectState = state => {
+        this.setState({ fileSelectOpen : state });
+    }
+
     render(){
         return(
-            <div className="chatroom">
-                <List style={{maxHeight : "85vh", overflowY:"scroll"}}>
-                    {
-                        this.state.messages.length 
-                        ? 
-                        this.state.messages.map(message => {
-                            return(
-                                <ListItem alignItems="flex-start">
-                                    <ListItemAvatar>
-                                    <Avatar alt={message.user.username} src={message.user.profilePicture} />
-                                    </ListItemAvatar>
-                                    <ListItemText
-                                    primary={message.title}
-                                    secondary={
-                                        <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            color="textPrimary"
-                                        >
-                                            {message.user.username}
-                                        </Typography>
-                                            - {message.text}
-                                        </React.Fragment>
-                                    }
-                                    />
-                                </ListItem>
-                            );
-                        })
-                        :
-                        null
-                    }
-                    <div ref={el => {this.messagesEnd = el; }}></div>
-                </List>
-                <div style={{position : "absolute", top : "90vh", left : "30%", right : "20%", width:"50%", maxHeight : "10vh"}}>
-                    <form onSubmit={this.addMessage} autoComplete="off" style={{ width : "100%",border : "1px solid black", display : "flex", flexWrap:"wrap"}}>
-                        <TextField
-                            id="outlined-name"
-                            label="Message Title (optional)"
-                            name="newMessageTitle"
-                            value={this.state.newMessageTitle}
-                            onChange={this.handleChange}
-                            margin="normal"
-                            variant="outlined"
-                            style={{width: "20%"}}
-                        />
-                        <TextField
-                            id="outlined-name"
-                            label="Message Text"
-                            name="newMessage"
-                            value={this.state.newMessage}
-                            onChange={this.handleChange}
-                            margin="normal"
-                            variant="outlined"
-                            style={{width: "70%"}}
-                        />
-                        <button style={{width : "10%"}} onClick={this.addMessage}>Send Message</button>
-                        <input onChange={this.setFile} type="file" />
-                    </form>
+            <React.Fragment>
+                <div className="chatroom">
+                    <List style={{maxHeight : "85vh", overflowY:"scroll"}}>
+                        {
+                            this.state.messages.length 
+                            ? 
+                            this.state.messages.map(message => {
+                                return(
+                                    <ListItem alignItems="flex-start">
+                                        <ListItemAvatar>
+                                        <Avatar alt={message.user.username} src={message.user.profilePicture} />
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                        primary={message.title}
+                                        secondary={
+                                            <React.Fragment>
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                color="textPrimary"
+                                            >
+                                                {message.user.username}
+                                            </Typography>
+                                                - {message.text}
+                                            </React.Fragment>
+                                        }
+                                        />
+                                    </ListItem>
+                                );
+                            })
+                            :
+                            null
+                        }
+                        <div ref={el => {this.messagesEnd = el; }}></div>
+                    </List>
+                    <div style={{position : "absolute", top : "90vh", left : "30%", right : "20%", width:"50%", maxHeight : "10vh"}}>
+                        <form onSubmit={this.addMessage} autoComplete="off" style={{ width : "100%",border : "1px solid black", display : "flex", flexWrap:"wrap"}}>
+                            <TextField
+                                id="outlined-name"
+                                label="Message Title (optional)"
+                                name="newMessageTitle"
+                                value={this.state.newMessageTitle}
+                                onChange={this.handleChange}
+                                margin="normal"
+                                variant="outlined"
+                                style={{width: "20%"}}
+                            />
+                            <TextField
+                                id="outlined-name"
+                                label="Message Text"
+                                name="newMessage"
+                                value={this.state.newMessage}
+                                onChange={this.handleChange}
+                                margin="normal"
+                                variant="outlined"
+                                style={{width: "70%"}}
+                            />
+                            <button style={{width : "10%"}} onClick={this.addMessage}>Send Message</button>
+                            <Button color="primary" onClick={() => this.handleFileSelectState(true)}>Upload File</Button>
+                        </form>
+                    </div>
                 </div>
-            </div>
+
+                {/* file select modal */}
+                <Dialog open={this.state.fileSelectOpen} onClose={() => this.handleFileSelectState(false)} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Upload File</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Upload a file to chat. Supported files PNG, JPG, PDF. (More coming soon)
+                        </DialogContentText>
+                        <input onChange={this.handleFileSelect} type="file" />
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={() => this.handleFileSelectState(false)} color="primary">
+                        Cancel
+                    </Button>
+                    <Button disabled={this.state.file === null} onClick={this.uploadFile} color="primary">
+                        Upload To Channel
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+            </React.Fragment>
         );
     }
 }
