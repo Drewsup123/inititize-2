fc21e1b840fcbcde1cfecd4f69c052f130d967d4
Drew <drewsup123c@gmail.com>
2019-09-17 12:57:41 -0600

    added board settings file

---
+++ initize/src/pages/BoardSettings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class BoardSettings {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Board Settings</h1>
+            </div>
+        )
+    }
+}
